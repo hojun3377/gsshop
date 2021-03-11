b825d22c6f1a47e7fa647fdcc14e5ccb1c5cacf1
hojun3377 <hojun3377@gmail.com>
2021-03-11 22:57:31 +0900

    Add subpage hero background animation js module

--- common/js/joint-gro.js
+++ common/js/joint-gro.js
@@ -4,23 +4,26 @@ const jointGro = (function() {
     //  b: beginning value
     //  c: change in value
     function easeOutQuart(t, b, c) {
-        return c * (1 - (1-t)*(1-t)*(1-t)*(1-t)) + b;
+        return c * (1 - (--t)*t*t*t) + b;
     }
 
     const tl = function(element, duration, vars) {
         var from = Number((element.style.objectPosition.split(' ')[1]).split('%')[0]);
         var to = Number((vars.split(' ')[1]).split('%')[0]);
-        var fps = 60;
-        var start = new Date().getTime();
+        var start = null;
 
-        var timer = setInterval(function() {
-            time = new Date().getTime() - start;
-            var p = easeOutQuart(time/1000, from, to-from);
+        var step = function(timestamp) {
+            if (!start) start = timestamp;
+
+            var progress = timestamp - start;
+            var p = easeOutQuart(progress/duration, from, to-from);
             element.style.objectPosition = '50% ' + p + '%';
-            if (time >= duration) {
-                clearInterval(timer);
+            if (progress < duration) {
+                window.requestAnimationFrame(step);
             }
-        }, 1000/fps);
+        }
+
+        window.requestAnimationFrame(step);
     }
 
     return {
@@ -29,31 +32,25 @@ const jointGro = (function() {
 
             window.addEventListener('scroll', function() {
                 var scrT = document.documentElement.scrollTop;
-                var duration = 2000; // 2s
+                var duration = 2000; // 10ms
 
                 if (scrT == 0) {
-                    // tl(heroImg, duration, "50% 0%");
-                    heroImg.style.objectPosition = "50% 0%";
+                    tl(heroImg, duration, "50% 0%");
                 }
                 else if (scrT < 200) {
-                    // tl(heroImg, duration, "50% 20%");
-                    heroImg.style.objectPosition = "50% 20%";
+                    tl(heroImg, duration, "50% 20%");
                 }
                 else if (scrT < 400) {
-                    // tl(heroImg, duration, "50% 40%");
-                    heroImg.style.objectPosition = "50% 40%";
+                    tl(heroImg, duration, "50% 40%");
                 }
                 else if (scrT < 600) {
-                    // tl(heroImg, duration, "50% 60%");
-                    heroImg.style.objectPosition = "50% 60%";
+                    tl(heroImg, duration, "50% 60%");
                 }
                 else if (scrT < 800) {
-                    // tl(heroImg, duration, "50% 80%");
-                    heroImg.style.objectPosition = "50% 80%";
+                    tl(heroImg, duration, "50% 80%");
                 }
                 else if (scrT < 1000) {
-                    // tl(heroImg, duration, "50% 100%");
-                    heroImg.style.objectPosition = "50% 100%";
+                    tl(heroImg, duration, "50% 100%");
                 }
                 else {
                     // 
