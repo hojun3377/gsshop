b57af443813f08ed40d0c21989e133c5854a753b
Hojun <hojun3377@gmail.com>
2021-03-17 00:31:37 +0900

    Implement hero animation(except auto)

--- common/js/main.js
+++ common/js/main.js
@@ -1,85 +1,96 @@
 const mainPage = (function() {
-
     var _windowW = window.innerWidth;
     var _windowH = window.innerHeight;
+    var _newWindowFlag = true;
 
     var getIndex = function(element) {
         for(var i = 0; i < element.parentNode.children.length; i++) {
-          if (element.parentNode.children[i] === element) {
-              return i;
-          }
+            if (element.parentNode.children[i] === element) {
+                return i;
+            }
         }
     }
 
-    var heroTxtsEffectAdd = function(element, index) {
-        var heroTxtsTxt = document.getElementsByClassName('hero-txts')[0].children[index];
+    var addClassName = function(element, className) {
+        if (element.className)
+            return;
 
-        switch(index) {
-            case 0:
-                element.parentNode.children[1].className += 'effect';
-                element.parentNode.children[2].className += 'effect';
-                break;
-            case 1:
-                element.parentNode.children[0].className += 'effect';
-                element.parentNode.children[2].className += 'effect';
-                break;
-            case 2:
-                element.parentNode.children[0].className += 'effect';
-                element.parentNode.children[1].className += 'effect';
-                break;
-            default:
-                //error
-        }
+        element.className = className;
+    }
+
+    var removeClassName = function(element, className) {
+        if (!element.className)
+            return;
         
-        heroTxtsTxt.style.display = "block";
+        element.classList.remove(className);
     }
 
-    var heroTxtsEffectRemove = function(element, index) {
-        var heroTxtsTxt = document.getElementsByClassName('hero-txts')[0].children[index];
+    var Sizing = function(videoEl, bgBox, txtBox) {
+        var pastWindowW = _windowW;
 
-        switch(index) {
-            case 0:
-                element.parentNode.children[1].classList.remove('effect');
-                element.parentNode.children[2].classList.remove('effect');
-                break;
-            case 1:
-                element.parentNode.children[0].classList.remove('effect');
-                element.parentNode.children[2].classList.remove('effect');
-                break;
-            case 2:
-                element.parentNode.children[0].classList.remove('effect');
-                element.parentNode.children[1].classList.remove('effect');
-                break;
-            default:
-                //error
-        }
-        
-        heroTxtsTxt.style.display = "none";
-    }
-
-    var heroSizing = function(bgBoxImg, txtBox) {
         _windowW = window.innerWidth;
         _windowH = window.innerHeight;
 
-        if (_windowW > 1024) bgBoxImg.style.opacity = 0;
-        else bgBoxImg.style.opacity = 1;
+
+        var heroTxts = txtBox.lastElementChild;
+
+        if (_newWindowFlag) {
+            TweenMax.to(txtBox.children[0], 1, {delay:0.3, top:0, opacity:1, ease:Power2.easeOut});
+            TweenMax.to(txtBox.children[1], 1, {delay:0.4, top:0, opacity:1, ease:Power2.easeOut});
+            TweenMax.to(txtBox.children[2], 1, {delay:0.5, top:0, opacity:1, ease:Power2.easeOut});
+        }
+
+        if (_windowW > 1024) {
+            for (i=0; i<bgBox.children.length; i++)
+                bgBox.children[i].style.opacity = 0;
+
+            for(i=0; i<txtBox.children.length-1; i++)
+                removeClassName(txtBox.children[i], 'effect');
+
+            for(i=0; i<heroTxts.children.length; i++) {
+                heroTxts.children[i].style.top = 50 + 'px';
+                heroTxts.children[i].style.opacity = 0;
+                heroTxts.children[i].style.display = 'none';
+            }
+            videoEl.play();
+        }
+        else if (_newWindowFlag || pastWindowW > 1024 && _windowW != pastWindowW) {
+            videoEl.pause();
+            bgBox.firstElementChild.style.opacity = 1;
+            
+            removeClassName(txtBox.firstElementChild, 'effect');
+            for(i=1; i<txtBox.children.length-1; i++)
+                addClassName(txtBox.children[i], 'effect');
+
+            heroTxts.firstElementChild.style.display = 'block';
+            TweenMax.to(heroTxts.firstElementChild, 1, {delay:0.7, top:0, opacity:1, ease:Power3.easeOut});
+        }
 
         if (_windowW > 768) txtBox.style.top = Math.floor(_windowH/2 - (txtBox.offsetHeight/2)) + 'px';
         else txtBox.style.top = 120 + 'px';
     }
 
     return {
-        resizeEvt: function() {
+        init: function() {
+            var heroVideo = document.getElementById('hero-video');
+            var bgBox = document.getElementsByClassName('bg-box')[0];
             var heroTxtBox = document.getElementsByClassName('hero-txt-box')[0];
-            var bgBoxImg = document.getElementsByClassName('bg-box')[0].children[0];
 
-            heroSizing(bgBoxImg, heroTxtBox);
+            Sizing(heroVideo, bgBox, heroTxtBox);
+            _newWindowFlag = false;
+        },
+
+        resizeEvt: function() {
+            var heroVideo = document.getElementById('hero-video');
+            var bgBox = document.getElementsByClassName('bg-box')[0];
+            var heroTxtBox = document.getElementsByClassName('hero-txt-box')[0];
+
             window.addEventListener('resize', () => {
-                heroSizing(bgBoxImg, heroTxtBox);
+                Sizing(heroVideo, bgBox, heroTxtBox);
             });
         },
 
-        heroTxtHover: function() {
+        mouseEvt: function() {
             var heroVideo = document.getElementById('hero-video');
             var heroTxtBoxA = document.getElementsByClassName('hero-txt-box')[0];
             var i = 0;
@@ -87,45 +98,66 @@ const mainPage = (function() {
             while(heroTxtBoxA.children[i] != heroTxtBoxA.lastElementChild) {
                 var el = heroTxtBoxA.children[i];
 
-                el.addEventListener('mouseover', function() {
+                el.addEventListener('mouseenter', function() {
                     var index = getIndex(this);
-                    var bgBoxImg = document.getElementsByClassName('bg-box')[0].children[index];
-                    
+                    var otherIndex = [((index+1) > 2) ? 0 : index+1, ((index-1) < 0) ? 2 : index-1];
+                    var parent = this.parentNode;
+                    var bgBox = document.getElementsByClassName('bg-box')[0];
+                    var heroTxts = document.getElementsByClassName('hero-txts')[0];
+
                     if(window.innerWidth > 1024) {
                         heroVideo.pause();
+                        addClassName(parent.children[otherIndex[0]], 'effect');
+                        addClassName(parent.children[otherIndex[1]], 'effect');
                     }
                     else {
-                        document.getElementsByClassName('bg-box')[0].children[0].style.opacity = 0;
+                        removeClassName(parent.children[index], 'effect');
+                        for(j=0; j<otherIndex.length; j++) {
+                            addClassName(parent.children[otherIndex[j]], 'effect');
+                            heroTxts.children[otherIndex[j]].style.top = 50 + 'px';
+                            heroTxts.children[otherIndex[j]].style.opacity = 1;
+                            heroTxts.children[otherIndex[j]].style.display = "none";
+                            TweenMax.to(bgBox.children[otherIndex[j]], 0.8, {opacity:0, ease:Power3.easeOut});
+
+                        }
                     }
-                    heroTxtsEffectAdd(this, index);
-                    bgBoxImg.style.opacity = 1;
+                    TweenMax.to(bgBox.children[index], 0.8, {opacity:1, ease:Power3.easeOut});
+                    heroTxts.children[index].style.display = "block";
+                    TweenMax.to(heroTxts.children[index], 0.5, {top:0, opacity:1, ease:Power3.easeOut});
                 });
 
-                el.addEventListener('mouseout', function() {
+                el.addEventListener('mouseleave', function() {
                     var index = getIndex(this);
+                    var otherIndex = [((index+1) > 2) ? 0 : index+1, ((index-1) < 0) ? 2 : index-1];
+                    var parent = this.parentNode;
                     var bgBoxImg = document.getElementsByClassName('bg-box')[0].children[index];
-
-                    bgBoxImg.style.opacity = 0;
-                    heroTxtsEffectRemove(this, index);
+                    var heroTxtsTxt = document.getElementsByClassName('hero-txts')[0].children[index];
+                    
                     if (window.innerWidth > 1024) {
+                        TweenMax.to(heroTxtsTxt, 0, {top:50, opacity:0, ease:Power3.easeOut});
+                        heroTxtsTxt.style.display = "none";
+                        TweenMax.to(bgBoxImg, 0.8, {opacity:0, ease:Power3.easeOut});
+                        removeClassName(parent.children[otherIndex[0]], 'effect');
+                        removeClassName(parent.children[otherIndex[1]], 'effect');
                         heroVideo.play();
-                    }
-                    else {
-                        document.getElementsByClassName('bg-box')[0].children[0].style.opacity = 1;
                     }
                 });
 
                 i++;
             }
+        },
+
+        auto: function() {
+
         }
     }
 })();
 
 document.addEventListener('DOMContentLoaded', function() {
-
+    mainPage.init();
 });
 
 window.addEventListener('load', function() {
     mainPage.resizeEvt();
-    mainPage.heroTxtHover();
+    mainPage.mouseEvt();
 })
