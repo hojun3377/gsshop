27d2d5c1dda800c7af8e883fc82d149ba1d0e218
hojun3377 <hojun3377@gmail.com>
2021-03-23 11:08:23 +0900

    Add joint-gro responsive hero img

--- common/js/subpage.js
+++ common/js/subpage.js
@@ -7,6 +7,32 @@ const subPage = (function () {
     let saTriggerHeight = 0;
     var saElementList;
 
+    //  t: current time
+    //  b: beginning value
+    //  c: change in value
+    function easeOutQuart(t, b, c) {
+        return c * (1 - (--t)*t*t*t) + b;
+    }
+
+    const tl = function(element, duration, vars) {
+        var from = Number((element.style.objectPosition.split(' ')[1]).split('%')[0]);
+        var to = Number((vars.split(' ')[1]).split('%')[0]);
+        var start = null;
+
+        var step = function(timestamp) {
+            if (!start) start = timestamp;
+
+            var progress = timestamp - start;
+            var p = easeOutQuart(progress/duration, from, to-from);
+            element.style.objectPosition = '50% ' + p + '%';
+            if (progress < duration) {
+                window.requestAnimationFrame(step);
+            }
+        }
+
+        window.requestAnimationFrame(step);
+    }
+
     // Scroll Animation (sa, 스크롤 애니메이션)
     const saFunc = function () {
         for (const element of saElementList) {
@@ -37,6 +63,35 @@ const subPage = (function () {
         scrollEvt: function () {
             saElementList = document.querySelectorAll('.sa');
             window.addEventListener('scroll', saFunc);
+
+            var heroImg = document.getElementById('hero-img');
+
+            window.addEventListener('scroll', function() {
+                var scrT = document.documentElement.scrollTop;
+                var duration = 2000; // 10ms
+
+                if (scrT == 0) {
+                    tl(heroImg, duration, "50% 0%");
+                }
+                else if (scrT < 200) {
+                    tl(heroImg, duration, "50% 20%");
+                }
+                else if (scrT < 400) {
+                    tl(heroImg, duration, "50% 40%");
+                }
+                else if (scrT < 600) {
+                    tl(heroImg, duration, "50% 60%");
+                }
+                else if (scrT < 800) {
+                    tl(heroImg, duration, "50% 80%");
+                }
+                else if (scrT < 1000) {
+                    tl(heroImg, duration, "50% 100%");
+                }
+                else {
+                    // 
+                }
+            });
         }
     }
 })();
